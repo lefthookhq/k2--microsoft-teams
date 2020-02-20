ed5ead348b93a0dca4b67fc5f0cb64cb769748cf
cbanister <cbanister@coderden.com>
2020-02-19 17:49:17 -0800

    test     executeTeamArchive

--- src/test.ts
+++ src/test.ts
@@ -24,69 +24,71 @@ test('rejects', async t => {
     t.is(error.message, '🦄');
 });
 
-// test('execute passes', async t => {
-//
-//     let xhr: {[key:string]: any} = null;
-//     class XHR {
-//         public onreadystatechange: () => void;
-//         public readyState: number;
-//         public status: number;
-//         public responseText: string;
-//         private recorder: {[key:string]: any};
-//
-//         constructor() {
-//             xhr = this.recorder = {};
-//             this.recorder.headers = {};
-//         }
-//
-//         open(method: string, url: string) {
-//             this.recorder.opened = {method, url};
-//         }
-//
-//         setRequestHeader(key: string, value: string) {
-//             this.recorder.headers[key] = value;
-//         }
-//
-//         send() {
-//             queueMicrotask(() =>
-//             {
-//                 this.readyState = 4;
-//                 this.status = 200;
-//                 this.responseText = JSON.stringify({
-//                     "id": 1234,
-//                     "displayName": "displayName"
-//                 });
-//                 this.onreadystatechange();
-//                 delete this.responseText;
-//             });
-//         }
-//     }
-//
-//     mock('XMLHttpRequest', XHR);
-//
-//     let result: any = null;
-//     function pr(r: any) {
-//         result = r;
-//     }
-//
-//     mock('postResult', pr);
-//
-//     await onexecute('team', 'get', {}, {"id": 1234});
-//
-//     // t.deepEqual(xhr, {
-//     //     opened: {
-//     //         method: 'GET',
-//     //         url: 'https://jsonplaceholder.typicode.com/todos/123'
-//     //     },
-//     //     headers: {
-//     //         'test': 'test value'
-//     //     }
-//     // });
-//
-//     t.deepEqual(result, {
-//         "id": 1234,
-//         "displayName": "displayName"
-//     });
-//
-//     t.pass();
-// });
+test('executeTeamArchive succeeds', async t => {
+
+    let xhr: {[key:string]: any} = null;
+    class XHR {
+        public onreadystatechange: () => void;
+        public readyState: number;
+        public status: number;
+        public responseText: string;
+        private recorder: {[key:string]: any};
+
+        constructor() {
+            xhr = this.recorder = {};
+            this.recorder.headers = {};
+        }
+
+        open(method: string, url: string) {
+            this.recorder.opened = {method, url};
+        }
+
+        setRequestHeader(key: string, value: string) {
+            this.recorder.headers[key] = value;
+        }
+
+        send() {
+            queueMicrotask(() =>
+            {
+                this.readyState = 4;
+                this.status = 200;
+                this.responseText = JSON.stringify({
+                    "id": 1234,
+                    "requestStatusUrl": undefined,
+                    "isSuccessful": true
+                });
+                this.onreadystatechange();
+                delete this.responseText;
+            });
+        }
+    }
+
+    mock('XMLHttpRequest', XHR);
+
+    let result: any = null;
+    function pr(r: any) {
+        result = r;
+    }
+
+    mock('postResult', pr);
+
+    await onexecute('team', 'archive', {}, {"id":1234});
+
+    // t.deepEqual(xhr, {
+    //     opened: {
+    //         method: 'GET',
+    //         url: 'https://jsonplaceholder.typicode.com/todos/123'
+    //     },
+    //     headers: {
+    //         'test': 'test value'
+    //     }
+    // });
+
+    t.deepEqual(result, {
+        "id": 1234,
+        "requestStatusUrl": undefined,
+        "isSuccessful": true
+    });
+
+    t.pass();
+});
